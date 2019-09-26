d2cb5cd15b5ffed8d301bd5b674714f1f7f98ffc
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2019-09-25 23:27:48 -0500

    commit inicial

---
+++ src/main/kotlin/mx/buap/fcc/clasificadorweb/ClasificadorWebApplication.kt
@@ -0,0 +1,11 @@
+package mx.buap.fcc.clasificadorweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClasificadorWebApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClasificadorWebApplication>(*args)
+}
---
+++ src/test/kotlin/mx/buap/fcc/clasificadorweb/ClasificadorWebApplicationTests.kt
@@ -0,0 +1,16 @@
+package mx.buap.fcc.clasificadorweb
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ClasificadorWebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
